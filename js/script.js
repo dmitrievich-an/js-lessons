09ba532f03b9560850a4ca5694bdce51de3efd8c
Artem <reebok001@yandex.ru>
2023-01-15 19:16:38 +0300

    finished the 2nd lesson

--- js/script.js
+++ js/script.js
@@ -1,26 +1,37 @@
-const students = [
-  {name: "Alex", age: 33, address: { street: "Pushkinskaya", number: 101, },},
-  {name: "Bob", age: 38, address: { street: "Sadovaya", number: 257, },},
+let students = [
+  {
+    name: "Bob",
+    age: 22,
+    isMarried: true,
+    scores: 85,
+  },
+  {
+    name: "Alex",
+    age: 21,
+    isMarried: true,
+    scores: 89,
+  },
+  {
+    name: "Nick",
+    age: 20,
+    isMarried: false,
+    scores: 120,
+  },
+  {
+    name: "John",
+    age: 19,
+    isMarried: false,
+    scores: 100,
+  }
 ];
 
-// const studentsCopy = students.map(s => ({...s}));
+const getStName = (array) => {
+  const result = [];
+  const getName = item => item.name
+  for (let i = 0; i < array.length; i++) {
+    result[i] = getName(array[i])
+  }
+  return result;
+}
 
-const studentsCopy = [...students]
-
-console.log(students);
-console.log(studentsCopy);
-
-console.log(students === studentsCopy); //false
-console.log(students[0] === studentsCopy[0]) //false
-
-console.log(students[0].address.street === studentsCopy[0].address.street);
-console.log(students[0].address.street);
-
-//CRUD
-const addSt = [...students, {name: "Anna", age: 28}];
-const updateBobAge = students.map(st => st.name === "Bob" ? {...st, age: 39} : st);
-const removeStudent = students.filter(st => st.name !== "Bob")
-
-console.log(students)
-console.log(updateBobAge)
-console.log(removeStudent)
+console.log(getStName(students));
