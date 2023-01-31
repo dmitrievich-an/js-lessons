a04898b8c80cb47fed7b0f07d566cce69538c3b2
Artem <reebok001@yandex.ru>
2023-02-01 00:26:13 +0300

    finished the 4th lesson

--- js/script.js
+++ js/script.js
@@ -1,18 +1,23 @@
-// handler === listener === subscriber => function
-// handler c callback
+const todoLists = [
+  {
+    id: "1",
+    title: "What to learn",
+    filter: "all",
+  },
+  {
+    id: "2",
+    title: "What to buy",
+    filter: "all",
+  },
+]
 
-const element = document.getElementById("hello")
-
-
-// element.addEventListener("click", listener)
-// element.removeEventListener("click", listener)
-
-
-// const string = "Hello My Best Friend!" // "H M B F"
-//
-// const onlyUpperCase = (s) => {
-//   return s.split("").filter(e => e === e.toUpperCase()).join("")
-// }
-//
-//
-// console.log(onlyUpperCase(string))
+const tasks = {
+  "1": [
+    {id: "12", name: "HTML", isDone: true},
+    {id: "13", name: "CSS", isDone: false},
+  ],
+  "2": [
+    {id: "24", name: "Meat", isDone: true},
+    {id: "22", name: "Milk", isDone: false},
+   ]
+}
